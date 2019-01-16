1768e6f0611221ee7858e10c04cb5a706b5920a0
Guillem <guillem@disedit.com>
2019-01-16 23:04:53 +0100

    Translate login pages

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -8,29 +8,51 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-
+                @if(Auth::user()->hasRole('credential_manager'))
+                    <li>
+                        <a class="nav-link {{ Route::currentRouteName() === 'credentials' ? 'active' : '' }}" href="{{ route('credentials') }}">
+                            <i class="far fa-id-card-alt"></i> Acreditaciones
+                        </a>
+                    </li>
+                @endif
+                @if(Auth::user()->hasRole('vote_manager'))
+                    <li>
+                        <a class="nav-link {{ Route::currentRouteName() === 'amendments' ? 'active' : '' }}" href="{{ route('amendments') }}">
+                            <i class="far fa-archive"></i> Votaciones
+                        </a>
+                    </li>
+                @endif
+                <li>
+                    <a class="nav-link {{ Route::currentRouteName() === 'vote' ? 'active' : '' }}" href="{{ route('vote') }}">
+                        <i class="far fa-hand-paper"></i> Votar
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link {{ Route::currentRouteName() === 'my-votes' ? 'active' : '' }}" href="{{ route('my-votes') }}">
+                        <i class="far fa-box-ballot"></i> Mis votos
+                    </a>
+                </li>
             </ul>
 
-            <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
                 @guest
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Entrar') }}</a>
                     </li>
                 @else
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                            <i class="far fa-user"></i> {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('my-votes') }}">
+                                <i class="far fa-box-ballot"></i> {{ __('Mis votos') }}
+                            </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                              document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                                onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                <i class="far fa-sign-out-alt"></i> {{ __('Cerrar sesión') }}
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
